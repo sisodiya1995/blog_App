3920b05880aa96c42ac50c1c6155018f88456554
Akash Singh <akashingh95@gmail.com>
2022-06-15 21:37:48 +0530

    error-boundary

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
 import { BrowserRouter } from "react-router-dom";
-import './stylesheet/style.css'
+import "./stylesheet/style.css";
+import ErrorBoundary from "./components/Errorboundry";
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </BrowserRouter>,
 
   document.getElementById("root")
